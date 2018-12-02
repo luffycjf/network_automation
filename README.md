1c47303b4c8df4c69cb27deb1d55c0e30484ddb9
root <luffycjf@sina.com>
2018-12-02 16:50:36 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # network_automation
 An ansible-based, multi-vendor network device configuration automatically generates and delivery system
+
+**下载文件**<br/>
+
+```javascript
+git clone https://github.com/luffycjf/network_automation
+```
+**安装ansible和模块**<br/>
+yum -y install ansible<br/>
+sed -i "s/#library/library/g" /etc/ansible/ansible.cfg<br/>
+mkdir -p /usr/share/my_modules/<br/>
+cd network_automation<br/>
+cp module/ssh_command.py /usr/share/my_modules/ <br/>
+如果已经安装了ansible，直接把module文件夹下的ssh_command.py放到ansible配置的模块目录下即可。<br/>
+
+**使用**<br/>
+目录下主文件network_automation.yml就是一个playbook，其中的含义[博客](jeffrycheng.com)上面都有说，两个task分别是生成配置文件和下发配置用的，写了一些配置模版和变量分别放在templates和vars文件夹中，目前时间和精力原因只更新了部分基础配置，不涉及interface和路由部分，后续会补充完整，有兴趣的也可以自己来写更多的。<br/>
+
+调用ansible-playbook network_automation.yml即可使用<br/>
+
+```javascript
+---
+- name: gengerate configuration
+  hosts: localhost
+  vars: 
+    model: h3c_v7
+    module: ntp
+    port: 22
+    username: "test"
+    password: "test"
+    command_interval: 0.5
+    vendor: GENERAL
+
+  vars_files:
+    - "vars/{{ model }}/{{ module }}.yml"
+
+  tasks:
+    - name: generate configuration
+      template:
+        src: "templates/{{ model }}/{{ module }}.j2"
+        dest: "config/{{ item.hostname }}.txt"
+      with_items:
+        - { hostname: 'h3c-v7'}
+
+    - name: config device
+      ssh_command:
+        port: "{{ port }}"
+        username: "{{ username }}"
+        password: "{{ password }}"
+        address: "{{ item.mgtip }}"
+        command_interval: "{{ command_interval }}"
+        command: ''
+        configfile: "config/{{ item.hostname }}.txt"
+      with_items:
+        - { hostname: 'h3c-v7', mgtip: '1.1.1.1' }
+```
+
+
+基于这套系统的网络架构规划，自动化网络变更等场景，我将会在后续文章中介绍。
---
+++ module/ssh_command.py
@@ -0,0 +1,239 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.0'}
+
+DOCUMENTATION = ''' 
+---
+module: ssh_command
+version_added: "1.0"
+short_description: run commands on network devices.
+description:
+     - run commands on network devices.
+options:
+  command:
+    description:
+      - network devices command,if you use configfile , thi can be ''
+    required: true
+  address:
+    description:
+      - remote network devices ip address
+    required: true
+  port:
+    description:
+      - remote network devices ssh port
+    required: false
+    default: 22
+  username:
+    description:
+      - remote network devices ssh username
+    required: true
+  password:
+    description:
+      - remote network devices ssh user password
+    required: true
+  configfile:
+    description:
+      - a config file,if you don't use it,this can be ''
+    required: false
+  command_interval:
+    description:
+      - the interval between two commands
+    required: False
+    default: 0.5
+  vendor:
+    description:
+      - the device's vendor(HUAWEI,CISCO,JUNIPER,GENERAL)
+    required: False
+    default: GENERAL
+author:
+    - "jeffrycheng"
+'''
+
+EXAMPLES = """
+---
+
+- hosts: localhost
+  gather_facts: no
+  vars:
+    port: 22
+    username: "tencent"
+    password: "tencent"
+    address: "192.168.157.132"
+    command_interval: 0.5
+    vendor: GENERAL
+
+  tasks:
+    - name: set ntp
+      ssh_command:
+        port: "{{ port }}"
+        address: "{{ address }}"
+        username: "{{ username }}"
+        password: "{{ password }}"
+        command_interval: "{{ command_interval }}"
+	vendor: {{ vendor }}
+        command: ''
+        configfile: /root/ansible/switch/cfg/ntp.txt
+
+    - name: display version
+      ssh_command:
+        port: 22
+        address: 192.168.1.1
+        username: test
+        password: test123
+        command_interval: 0.1
+        vendor: HUAWEI
+        command: 'display version'
+        configfile: ''
+"""
+
+RETUN = """
+stdout:
+  description: the set of responses from the commands
+  returned: always
+  type: list
+  sample: ['...', '...']
+
+command:
+  description: run command
+  returned: always
+  type: str
+  sample: display version
+"""
+
+
+
+import time
+import re
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_bytes
+from ansible.errors import AnsibleError, AnsibleConnectionFailure
+
+
+stdmore = re.compile(r"-[\S\s]*[Mm]ore[\S\s]*-")
+
+general_endcondition = re.compile(r"\S+[#>\]]\s*[\n\r]")
+hw_endcondition = re.compile(r"\S+[>\]]\s*[\n\r]")
+cisco_endcondition = re.compile(r"\S+#\s*[\n\r]")
+juniper_endcondition = re.compile(r"\S+[#\]]\s*[\n\r]")
+
+
+try:
+  import paramiko
+except ImportError:
+  raise AnsibleError("paramiko is not installed, please use pip install paramiko")
+
+
+class ssh_comm(object):
+    def __init__(self,address,username,password,port=22):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.connect(address, port=port, username=username, password=password, look_for_keys=False,allow_agent=False)
+        self.shell = self.client.invoke_shell()
+        while True:
+            time.sleep(0.5)
+            if self.shell.recv_ready() or self.shell.recv_stderr_ready():
+                break
+        self.shell.recv('')
+    def recv_all(self,interval,condition_type='GENERAL'):
+	if condition_type == 'HUAWEI':
+	    condition = hw_endconditio
+	elif condition_type == 'CISCO':
+	    condition = cisco_endcondition
+	elif condition_type == 'JUNIPER':
+	    condition = juniper_endcondition
+	else:
+	    condition = general_endcondition
+	output = ''
+	self.shell.send('\n')
+	while True:
+	    while True:
+		time.sleep(interval)
+		if self.shell.recv_ready() or self.shell.recv_stderr_ready():
+		    break
+	    output += self.shell.recv(4096)
+	    if stdmore.findall(output):
+		break
+	    elif condition.findall(output):
+		output = '\r\n'.join(output.split('\n')[1:-2])
+		break
+	return output
+    def send_command(self,command_interval,command,endcondition):
+        command += "\n"
+        self.shell.send(command)
+        stdout = self.recv_all(command_interval)
+        data = stdout.split('\n')
+        while stdmore.findall(data[-1]):
+            self.shell.send(" ")
+            while True:
+                time.sleep(command_interval)
+                if self.shell.recv_ready() or self.shell.recv_stderr_ready():
+                    break
+            tmp = self.recv_all(interval=command_interval,condition_type=endcondition)
+            data = tmp.split('\n')
+            stdout += tmp
+        return stdout
+    def close(self):
+	if self.client is not None:
+	    self.client.close()
+    def run(self,cmds,command_interval,endcondition):
+	stderr = ['^','ERROR','Error','error','invalid','Invalid','Ambiguous','ambiguous']
+        stdout = ''
+        rc = 0
+        for cmd in cmds.split('\n'):
+            stdout += self.send_command(command=cmd,command_interval=command_interval,endcondition=endcondition)
+        for err in stderr:
+            if err in stdout:
+		rc = 1
+        return rc, stdout
+            
+            
+def main():
+    module = AnsibleModule(
+    argument_spec = dict(
+    configfile=dict(required=False,type='str'),
+    command=dict(required=True, type='str'),
+    address=dict(required=True, type='str'),
+    port=dict(required=False, type='int', default=22),
+    username=dict(required=True, type='str'),
+    password=dict(required=True, type='str', no_log=True),
+    command_interval=dict(required=False, type='float',default=0.5),
+    vendor=dict(required=False, type='str',default='general')),
+    supports_check_mode=True)
+    command = to_bytes(module.params['command'], errors='surrogate_or_strict')
+    configfile = to_bytes(module.params['configfile'], errors='surrogate_or_strict')
+    address = to_bytes(module.params['address'], errors='surrogate_or_strict')
+    username = to_bytes(module.params['username'], errors='surrogate_or_strict')
+    password = to_bytes(module.params['password'], errors='surrogate_or_strict')
+    command_interval = to_bytes(module.params['command_interval'], errors='surrogate_or_strict')
+    vendor = to_bytes(module.params['vendor'], errors='surrogate_or_strict')
+    result = {'changed': False}
+    if configfile != '':
+        try:
+            with open(configfile) as f:
+                command = f.read()
+            command = to_bytes(command, errors='surrogate_or_strict')
+        except Exception as e:
+            raise AnsibleError('Config file is wrong\n' + str(e) )
+    try:
+        connection = ssh_comm(address=address, username=username, password=password, port=module.params['port'])
+    except Exception as e:
+        raise AnsibleConnectionFailure(str(e))
+    try:
+        rc,stdout = connection.run(cmds=command,command_interval=float(command_interval),endcondition=vendor.upper())
+    except Exception as e:
+        raise AnsibleError('Exec command error.\n' + str(e))
+
+    if rc == 0:
+        result['changed'] = True
+    elif rc == 1:
+        module.fail_json(msg=stdout)
+    connection.close()
+    result.update({'command': command,'rc': rc,'stdout': stdout})
+    module.exit_json(**result)
+
+if __name__ == '__main__':  
+    main() 
